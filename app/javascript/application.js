4dacb690541bdfce97c256a614c6cb508a4f87fe
CAROLINARUIZG <mcruiz2@miuandes.cl>
2026-03-26 10:31:36 -0300

    lab-01-changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", () => {
+  const elBoton = document.getElementById('boton');
+  const elInput = document.getElementById('palabras');
+  const elParrafo = document.getElementById('resumen-texto');
+
+  if (elBoton) {
+    elBoton.addEventListener('click', () => {
+      const nuevoTexto = elInput.value;
+
+      if (nuevoTexto.trim() !== "") {
+        elParrafo.innerText = nuevoTexto;
+        elInput.value = ""; 
+      } else {
+        alert("Escribe algo:)");
+      }
+    });
+  }
+});
